d17b890f0be757fbba3a3f469db6a60817d7e479
Asher <asher_github@163.com>
2023-05-15 11:35:54 +0800

    created text_utils - implement compute_text_similarity

--- models/utils/text_utils.py
+++ models/utils/text_utils.py
@@ -1,19 +1,27 @@
-from nltk import sent_tokenize
-from sentence_transformers import SentenceTransformer,util
+from nltk.corpus import stopwords
+from nltk import sent_tokenize, word_tokenize
+from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def compute_text_similarity(sentence1:str,sentence2:str):
+
+def compute_text_similarity(sentence1: str, sentence2: str):
     sentence1 = sent_tokenize(preprocess_sentence(sentence1))
     sentence2 = sent_tokenize(preprocess_sentence(sentence2))
 
-    embedding1 = model.encode(sentences=sentence1,convert_to_tensor=True)
-    embedding2 = model.encode(sentences=sentence2,convert_to_tensor=True)
+    embedding1 = model.encode(sentences=sentence1, convert_to_tensor=True)
+    embedding2 = model.encode(sentences=sentence2, convert_to_tensor=True)
 
-    cosine_scores = util.cos_sim(embedding1,embedding2)
+    cosine_scores = util.cos_sim(embedding1, embedding2)
+
+    return cosine_scores
 
 
-# filter stop word & truncate sentence length
-def preprocess_sentence(sentence:str):
-    pass
+# filter stop word & truncate sentence length ;return torch
+def preprocess_sentence(sentence: str):
+    before_words = word_tokenize(sentence)
+    after_words = [
+        word for word in before_words if word not in stopwords.words("english")]
+    sentence_ = " ".join(after_words)
+    return sentence_
 
