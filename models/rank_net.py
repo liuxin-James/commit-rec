740329e9695cb2ed87bbde04c0b0c562dad93fc1
asher <asher_github@163.com>
2023-05-21 00:04:53 +0800

    build Bert model to handle text information - added BertModel to handle commit message - build Wide&Deep net structrue

--- models/rank_net.py
+++ models/rank_net.py
@@ -1,6 +1,82 @@
+import torch
+import numpy as np
 import torch.nn as nn
 
-from transformers import BertModel
+from torch import Tensor, nn
+from transformers import BertModel,DistilBertModel,DistilBertTokenizer
+from pytorch_widedeep.utils.fastai_transforms import (
+    fix_html,
+    spec_add_spaces,
+    rm_useless_spaces,
+)
+
+class BertTokenizer(object):
+    def __init__(
+        self,
+        pretrained_tokenizer="distilbert-base-uncased",
+        do_lower_case=True,
+        max_length=90,
+    ):
+        super(BertTokenizer, self).__init__()
+        self.pretrained_tokenizer = pretrained_tokenizer
+        self.do_lower_case = do_lower_case
+        self.max_length = max_length
+
+    def fit(self, texts):
+        self.tokenizer = DistilBertTokenizer.from_pretrained(
+            self.pretrained_tokenizer, do_lower_case=self.do_lower_case
+        )
+
+        return self
+
+    def transform(self, texts):
+        input_ids = []
+        for text in texts:
+            encoded_sent = self.tokenizer.encode_plus(
+                text=self._pre_rules(text),
+                add_special_tokens=True,
+                max_length=self.max_length,
+                padding="max_length",
+                truncation=True,
+            )
+
+            input_ids.append(encoded_sent.get("input_ids"))
+        return np.stack(input_ids)
+
+    def fit_transform(self, texts):
+        return self.fit(texts).transform(texts)
+
+    @staticmethod
+    def _pre_rules(text):
+        return fix_html(rm_useless_spaces(spec_add_spaces(text)))
+
+class BertModel(nn.Module):
+    def __init__(
+        self,
+        model_name: str = "distilbert-base-uncased",
+        freeze_bert: bool = False,
+    ):
+        super(BertModel, self).__init__()
+
+        self.bert = DistilBertModel.from_pretrained(
+            model_name,
+        )
+
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, X_inp: Tensor) -> Tensor:
+        attn_mask = (X_inp != 0).type(torch.int8)
+        outputs = self.bert(input_ids=X_inp, attention_mask=attn_mask)
+        return outputs[0][:, 0, :]
+
+    @property
+    def output_dim(self) -> int:
+        # This is THE ONLY requirement for any model to work with pytorch-widedeep. Must
+        # have a 'output_dim' property so the WideDeep class knows the incoming dims
+        # from the custom model. in this case, I hardcoded it
+        return 768
 
 
 class DeepBert(nn.Module):
--- models/wd_train.py
+++ models/wd_train.py
@@ -1,4 +1,13 @@
+import pandas as pd
+import numpy as np
 
+from rank_net import BertModel,BertTokenizer
+from sklearn.model_selection import train_test_split
+
+from pytorch_widedeep import Trainer
+from pytorch_widedeep.metrics import Accuracy
+from pytorch_widedeep.models import Wide, WideDeep
+from pytorch_widedeep.preprocessing import WidePreprocessor
 
 
 def do_train():
@@ -9,7 +18,26 @@ def do_eval():
 
 
 if __name__ == "__main__":
+    df = pd.read_csv()
+    df_train, df_test = train_test_split(df, test_size=0.2, stratify=df.income_label)
 
-    pass
+    wide_cols = []
+    crossed_cols = []
+    # wide data preprocess
+    wide_preprocessor = WidePreprocessor(wide_cols=wide_cols, crossed_cols=crossed_cols)
+    X_wide = wide_preprocessor.fit_transform(df_train)
+
+    # deep data preprocess
+    bert_tokenizer = BertTokenizer()
+    X_bert_tr = bert_tokenizer.fit_transform(df_train["review_text"].tolist())
+    X_bert_te = bert_tokenizer.transform(df_test["review_text"].tolist())
+
+    # build model
+    wide = Wide(input_dim=np.unique(X_wide).shape[0], pred_dim=1)
+    bert_model = BertModel(freeze_bert=True)
+
+    model = WideDeep(wide=wide, deeptabular=bert_model,head_hidden_dims=[256, 128, 64],pred_dim=2)
+
+    trainer = Trainer(model, objective="binary", metrics=[Accuracy])
 
 
