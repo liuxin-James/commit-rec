8acf343de9163a5cfa1d90ece0d7eb4afddeae79
asher <asher_github@163.com>
2023-05-21 16:05:34 +0800

    updated model training process

--- models/wd_train.py
+++ models/wd_train.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from rank_net import BertModel,BertTokenizer
+from rank_net import BertModel, BertTokenizer
 from sklearn.model_selection import train_test_split
 
 from pytorch_widedeep import Trainer
-from pytorch_widedeep.metrics import Accuracy
+from pytorch_widedeep.metrics import Accuracy, Precision
 from pytorch_widedeep.models import Wide, WideDeep
 from pytorch_widedeep.preprocessing import WidePreprocessor
 
@@ -13,31 +13,52 @@ from pytorch_widedeep.preprocessing import WidePreprocessor
 def do_train():
     pass
 
+
 def do_eval():
     pass
 
 
 if __name__ == "__main__":
-    df = pd.read_csv()
-    df_train, df_test = train_test_split(df, test_size=0.2, stratify=df.income_label)
+    # data part
+    path = "./train.csv"
+    df = pd.read_csv(path)
+    df_train, df_test = train_test_split(
+        df, test_size=0.2, stratify=df.income_label)
 
-    wide_cols = []
-    crossed_cols = []
+    wide_cols = ["share_files_nums", "share_files_rate", "only_commit_files_nums",
+                 "exist_cve", "insert_loc_nums", "delete_loc_nums", "all_loc_nums", "all_method_nums"]
+    crossed_cols = [("delete_loc_nums", "all_loc_nums"), ("insert_loc_nums",
+                                                          "all_loc_nums"), ("share_files_nums", "share_files_rate")]
+
+    target = "is_right"
+    target = df_train[target].values
+
     # wide data preprocess
-    wide_preprocessor = WidePreprocessor(wide_cols=wide_cols, crossed_cols=crossed_cols)
+    wide_preprocessor = WidePreprocessor(
+        wide_cols=wide_cols, crossed_cols=crossed_cols)
     X_wide = wide_preprocessor.fit_transform(df_train)
 
-    # deep data preprocess
+    # deep training data preprocess
     bert_tokenizer = BertTokenizer()
-    X_bert_tr = bert_tokenizer.fit_transform(df_train["review_text"].tolist())
-    X_bert_te = bert_tokenizer.transform(df_test["review_text"].tolist())
+    X_bert_tr = bert_tokenizer.fit_transform(df_train["cve_desc"].tolist())
 
-    # build model
+    # model part-> build model
     wide = Wide(input_dim=np.unique(X_wide).shape[0], pred_dim=1)
     bert_model = BertModel(freeze_bert=True)
 
-    model = WideDeep(wide=wide, deeptabular=bert_model,head_hidden_dims=[256, 128, 64],pred_dim=2)
+    model = WideDeep(wide=wide, deeptext=bert_model,
+                     head_hidden_dims=[256, 128, 64], pred_dim=2)
 
-    trainer = Trainer(model, objective="binary", metrics=[Accuracy])
+    trainer = Trainer(model, objective="binary", metrics=[Precision])
 
+    trainer.fit(
+        X_wide=X_wide,
+        X_text=X_bert_tr,
+        target=target,
+        n_epochs=5,
+        batch_size=256,
+    )
 
+    # deep testing data preprocess
+    X_wide_te = wide_preprocessor.transform(df_test)
+    X_bert_te = bert_tokenizer.transform(df_test["cve_desc"].tolist())
