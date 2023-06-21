776b3486e6cf98f763a347cc64c2e9331ddbd776
Asher <asher_github@163.com>
2023-06-21 17:16:56 +0800

    updated rec_models - updated run:main

--- rec_models/run.py
+++ rec_models/run.py
@@ -12,3 +12,15 @@ def predict():
     pass
 
 
+def main():
+    # read data
+    # build dataset
+    # resampling using SMOTE
+    # build Dataloader
+
+    # to handle sparse features
+    # build widedeep architecture model
+
+    # model training
+    
+    pass
